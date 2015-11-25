65d5d23cdabb060f186ce1bbb1eb3e9e37cf5377
Richard Bangay <bangar01@mc-s083313.local>
2015-11-25 11:45:14 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# 12791: Whats_the_world_listening_to
+
+Project Kelvin - Full page width - Scaffold
+
+## Getting started
+
+Set up the project
+
+```
+grunt
+```
+
+Make images responsive
+
+```
+grunt images
+```
+
+Build World Service version
+
+```
+grunt translate
+```
+
+## Kelvin scaffold
+
+This full frame project was built using the Kelvin scaffold v0.1.0
+
+## License
+Copyright (c) 2014 BBC
---
+++ source/js/app.js
@@ -0,0 +1,22 @@
+define(['lib/news_special/bootstrap', 'snippets'], function (news, snippets) {
+
+	/*
+	 * variable declarations
+	*/
+
+	/*
+	 * init stuff
+	*/
+    news.sendMessageToremoveLoadingImage();
+
+    snippets.init({
+        // lang: "en",
+        // continuous: false,
+        // base_url: "/modules/snippet",
+        // pause_enabled: true,
+        // uk: null,
+        // context: null
+    });
+
+
+});
