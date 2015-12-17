f01cce868bf48ff36af58947650d9dc3546b8425
Punit Shah <punit.shah@bbc.co.uk>
2015-12-17 11:44:54 +0000

    share for cities with no twin town

--- source/js/view/shareToolsView.js
+++ source/js/view/shareToolsView.js
@@ -10,21 +10,31 @@ define(['lib/news_special/bootstrap', 'lib/news_special/share_tools/controller']
         news.pubsub.on('display-share-tools', displayShareTools);
     };
 
-    var displayShareTools = function (cityName, twinTownName) {
+    var displayShareTools = function (cityName, twinTown) {
         if (shareTools === null) {
-            createShareTools(cityName, twinTownName);
+            if (twinTown) {
+                createShareTools(cityName, twinTown.name);
+            } else {
+                createShareTools(cityName);
+            }
         } else {
             //sharetools has already been created
             //destroy and try again
             destroyShareTools();
-            displayShareTools(cityName, twinTownName);
+            displayShareTools(cityName, twinTown);
         }
     };
 
     var createShareTools = function (cityName, twinTownName) {
-        var message = $shareToolsHolder.attr('data-share-message')
-            .replace('{{cityName}}', cityName)
-            .replace('{{twinTownName}}', twinTownName);
+        var message;
+        if (twinTownName) {
+            message = $shareToolsHolder.attr('data-share-message')
+                .replace('{{cityName}}', cityName)
+                .replace('{{twinTownName}}', twinTownName);
+        } else {
+            message = $shareToolsHolder.attr('data-share-message-notwin')
+                .replace('{{cityName}}', cityName);
+        }
         var shareToolsOptions = {
             'header': 'Share',
             'message': message,
