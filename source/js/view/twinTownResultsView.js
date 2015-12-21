46a850e743d6813214a478d703455f7bc883db22
Punit Shah <punit.shah@bbc.co.uk>
2015-12-21 17:32:04 +0000

    if only 1 track in common, text is singular
    'One track in common', not 'One tracks in common'

--- source/js/view/twinTownResultsView.js
+++ source/js/view/twinTownResultsView.js
@@ -18,6 +18,7 @@ define(['lib/news_special/bootstrap', 'utils'], function (news, utils) {
         $twinTownMilesAway = news.$('#ns12791_twinTownDistance_miles');
         $twinTownKmAway = news.$('#ns12791_twinTownDistance_km');
         $tracksInCommonNumber = news.$('#ns12791_tracksInCommon_number');
+        $tracksInCommonText = news.$('#ns12791_tracksInCommon_text');
         $tracksInCommonList = news.$('.ns12791_tracksInCommon_list');
         $twinTownFound = news.$('.ns12791_twinTownFound');
         $noTwinTown = news.$('.ns12791_noTwinTown');
@@ -64,6 +65,12 @@ define(['lib/news_special/bootstrap', 'utils'], function (news, utils) {
         };
 
         $tracksInCommonNumber.text(convertNumberToWord(commonTracksArray.length));
+        if (commonTracksArray.length === 1) {
+            $tracksInCommonText.text($tracksInCommonText.attr('data-singular'));
+        } else {
+            $tracksInCommonText.text($tracksInCommonText.attr('data-plural'));
+        }
+
         $tracksInCommonList.empty();
         news.$.each(commonTracksArray, function (index, value) {
             var li = $('<li>').text(value).appendTo($tracksInCommonList);
